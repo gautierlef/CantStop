d4f979ffc663c87c6b778ed36a82a7d54b452748
gautierlef <gautier.leffebvre@gmail.com>
2023-02-13 16:37:14 +0100

    Debugging

--- cantStop.py
+++ cantStop.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
         print('Turn : ' + str(turn))
         climber = 3
         stop = False
-        while climber > 0 and stop is False:
+        current_climbers_position = []
+        while stop is False:
             print('Climber remaining : ' + str(climber))
             stop = random.choice([False, True])
             rolls = get_rolls()
@@ -28,11 +29,15 @@ if __name__ == '__main__':
                 if len(choices) == 1:
                     for choice in choices[0]:
                         boards[player_turn].put_climber(choice)
-                        climber -= 1
+                        if climber not in current_climbers_position and climber > 0:
+                            climber -= 1
+                            current_climbers_position.append(choice)
                 else:
                     for choice in choices[random.randrange(0, len(choices))]:
                         boards[player_turn].put_climber(choice)
-                        climber -= 1
+                        if climber not in current_climbers_position and climber > 0:
+                            climber -= 1
+                            current_climbers_position.append(choice)
                 boards[player_turn].show_board()
             else:
                 boards[player_turn].remove_climbers()
